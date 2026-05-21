0e469483a4f0fea6783fa2e18bc7d0ee49e280f4
Birgitt Thomsen <birgitt.thomsen@t-online.de>
2026-05-21 22:03:51 +0200

    refactor code

--- filter_users.py
+++ filter_users.py
@@ -1,10 +1,15 @@
+""" Program to filter users by name and age """
+
 import json
 
 
-def filter_users_by_name(name):
+def load_data():
     with open("users.json", "r") as file:
-        users = json.load(file)
+        return json.load(file)
 
+
+def filter_users_by_name(data, name):
+    """ Filter users by name """
     filtered_users = [user for user in users if
                       user["name"].lower() == name.lower()]
 
@@ -12,10 +17,8 @@ def filter_users_by_name(name):
         print(user)
 
 
-def filter_users_by_age(age):
-    with open("users.json", "r") as file:
-        users = json.load(file)
-
+def filter_users_by_age(data, age):
+    """ Filter users by age """
     filtered_users = [user for user in users if
                       user["age"] == int(age)]
 
@@ -23,10 +26,8 @@ def filter_users_by_age(age):
         print(user)
 
 
-def filter_users_by_email(email):
-    with open("users.json", "r") as file:
-        users = json.load(file)
-
+def filter_users_by_email(data, email):
+    """ Filter users by email """
     filtered_users = [user for user in users if
                       user["email"].lower() == email.lower()]
 
@@ -36,17 +37,19 @@ def filter_users_by_email(email):
 
 if __name__ == "__main__":
     filter_option = input(
-        "What would you like to filter by? (Currently, only 'name', "
-        "'age' and 'email' are supported): ").strip().lower()
+        "What would you like to filter by? (Fields 'name', 'age' and 'email "
+        "are supported): ").strip().lower()
+
+    users = load_data()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
-        filter_users_by_name(name_to_search)
+        filter_users_by_name(users, name_to_search)
     elif filter_option == "age":
         age_to_search = input("Enter an age to filter users: ").strip()
-        filter_users_by_age(age_to_search)
+        filter_users_by_age(users, age_to_search)
     elif filter_option == "email":
         email_to_search = input("Enter an email to filter users: ").strip()
-        filter_users_by_email(email_to_search)
+        filter_users_by_email(users, email_to_search)
     else:
         print("Filtering by that option is not yet supported.")
